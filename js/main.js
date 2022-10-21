fcc6f18a46f5b001368780ac6742de8edc941f0d
Cody Kirschbaum <baumerc10@gmail.com>
2022-10-20 20:51:43 -0400

    added BMI API

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// BMI API
+// const data = null;
+
+// const xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
+
+// xhr.addEventListener("readystatechange", function () {
+// 	if (this.readyState === this.DONE) {
+// 		console.log(this.responseText);
+// 	}
+// });
+
+// xhr.open("GET", "https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=150&height=1.83");
+// xhr.setRequestHeader("X-RapidAPI-Key", "SIGN-UP-FOR-KEY");
+// xhr.setRequestHeader("X-RapidAPI-Host", "body-mass-index-bmi-calculator.p.rapidapi.com");
+
+// xhr.send(data);
